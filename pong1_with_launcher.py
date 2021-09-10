a172bfdef21274fc0db22ce0bdd227120d13fa4d
Pythonic456 <noah.horler@gmail.com>
2021-09-10 17:56:16 +0100

    Added Tk error catching (@pysquared), and a better UI

--- pong1_with_launcher.py
+++ pong1_with_launcher.py
@@ -5,6 +5,16 @@ import time, random, math
 root = tk.Tk()
 root.title('Pong V1 - Launcher')
 
+
+#Enable catching of errors in Tk loops
+def _CallWrapper__call__notrap(self, *args):
+	if self.subst:
+		args = self.subst(*args)
+	return self.func(*args)
+tk.CallWrapper.__call__ = _CallWrapper__call__notrap
+
+
+
 def launch_game(FPS_LIMIT, FRAME_SLEEP_OFFSET, SCREEN_RESOLUTION, BOUNCE_X, BALL_BOUNCE_SPEED, root):
     #FPS_LIMIT: 60 is recommended
     #FRAME_SLEEP_OFFSET: If this is 0, the program will do its best to reach the FPS_LIMIT
@@ -23,6 +33,11 @@ def launch_game(FPS_LIMIT, FRAME_SLEEP_OFFSET, SCREEN_RESOLUTION, BOUNCE_X, BALL
     score = 0
     move_speed_float = MOVE_SPEED
 
+    for i in range(score):
+        #Only for use if you change the score to start with (for instance, to have faster ball speeds to begin with)
+        move_speed_float += 0.2
+        MOVE_SPEED = math.floor(move_speed_float)
+
     class window:
         def __init__(self, title, root):
             self.toplevel = tk.Toplevel(root)
@@ -39,7 +54,7 @@ def launch_game(FPS_LIMIT, FRAME_SLEEP_OFFSET, SCREEN_RESOLUTION, BOUNCE_X, BALL
     playarea_right_edge = window('Right Edge', root)
     playarea_left_edge = window('Left Edge', root)
     playarea_bottom_edge = window('Bottom Edge', root)
-    playarea_top_edge = window('Play Area', root)
+    playarea_top_edge = window('No hit area', root)
     score_window = window('Score', root)
 
     score_label = tk.Label(score_window.toplevel, text=str(score))
@@ -47,8 +62,8 @@ def launch_game(FPS_LIMIT, FRAME_SLEEP_OFFSET, SCREEN_RESOLUTION, BOUNCE_X, BALL
     score_label.config(font=("Courier", 44))
 
     score_window.toplevel.geometry('200x100+'+str(SCREEN_RESOLUTION[0]-200)+'+35')
-    playarea_right_edge.toplevel.geometry('5x'+str(SCREEN_RESOLUTION[1]-50)+'+'+str(SCREEN_RESOLUTION[1])+'+35')
-    playarea_left_edge.toplevel.geometry('5x'+str(SCREEN_RESOLUTION[1])+'+0+0')
+    playarea_right_edge.toplevel.geometry('5x'+str(SCREEN_RESOLUTION[1]-35)+'+'+str(SCREEN_RESOLUTION[1])+'+35')
+    playarea_left_edge.toplevel.geometry('5x'+str(SCREEN_RESOLUTION[1]-50)+'+0+0')
     playarea_bottom_edge.toplevel.geometry(str(SCREEN_RESOLUTION[1])+'x5+0+'+str(SCREEN_RESOLUTION[1]))
     playarea_top_edge.toplevel.geometry(str(SCREEN_RESOLUTION[1])+'x5+0+0')
 
@@ -122,7 +137,14 @@ def launch_game(FPS_LIMIT, FRAME_SLEEP_OFFSET, SCREEN_RESOLUTION, BOUNCE_X, BALL
     except:
         root.quit()
         print()
-        print('Score:', str(score))
+        print('Seems to have crashed, here is your score:', str(score))
+    ball.toplevel.destroy()
+    playarea_left_edge.toplevel.destroy()
+    playarea_right_edge.toplevel.destroy()
+    playarea_top_edge.toplevel.destroy()
+    playarea_bottom_edge.toplevel.destroy()
+    score_window.toplevel.destroy()
+    return score
 
 
 screen_res_var = tk.StringVar(root)
@@ -143,7 +165,14 @@ def go():
     fps_dropmenu.destroy()
     res_dropmenu.destroy()
     go_button.destroy()
-    launch_game(int(fps_var.get()), 0, resolutions[screen_res_var.get()], resolutions[screen_res_var.get()][1], 2, root)
+    score = launch_game(int(fps_var.get()), 0, resolutions[screen_res_var.get()], resolutions[screen_res_var.get()][1], 2, root)
+    score_label = tk.Label(root, text='Score: '+str(score))
+    score_label.config(font=("Courier", 88))
+    score_label.pack()
+    root.deiconify()
+    root.title('Pong V1')
+    root.geometry('+'+str(int(resolutions[screen_res_var.get()][0]/2))+'+'+str(int(resolutions[screen_res_var.get()][1]/2)))
+    root.focus()
 
 go_button = tk.Button(root, text='Start', command=go)
 go_button.pack()
